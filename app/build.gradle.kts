27fc7dce72de978e49a42bd43b22233996b15551
japygo <hyunnjinn@gmail.com>
2025-12-19 09:47:04 +0900

    chore: Bump versionCode to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.japygo.study.randomcolorpicker"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
